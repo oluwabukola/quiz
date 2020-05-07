e30ade271d8baa33cdc175c3fb355898d701f96c
oluwabukola <daniellabaiyewu@gmail.com>
2020-05-07 17:58:40 +0100

    addition

--- script.js
+++ script.js
@@ -54,45 +54,85 @@ let questions = [
         correctAnswer: "A"
     }
 ];
-let quizContainer = document.querySelectorAll(".quizcontainer");
-let questionContainer = document.querySelector('.questionbox');
-let answerContainer = document.querySelectorAll(".btn");
-let submitButon = document.querySelector(".submit");
-let answer;
-let output;
-function quiz() {
 
-     output = [];
+// const quizContainer = document.getElementById('quiz');
+// const resultsContainer = document.getElementById('results');
+
+
+// const submitButton = document.getElementById('submit');
+const quizContainer = document.querySelectorAll(".container");
+const submitButon = document.querySelector(".submit");
+
+function quiz() {
+    
+     const output = [];
     //For each question
-    questions.forEach((currentQuestion, questionNumber) => {
-        answer= [];
+    questions.forEach(
+                      (currentQuestion, questionNumber) => {
+       const  answer= [];
         //for each aveilable answer
-        for (option in currentQuestion.answer) {
-            answer.push(`${currentQuestion.answer[option]}`);
+        for (letter in currentQuestion.answer) {
+            answer.push(
+                
+                `<div id="btn" onclick="pickAnswer(${questionNumber}, ${letter}) >
+                  ${currentQuestion.answer[letter]}
+                </div>`
+                
+              );
         }
-         //adding thw question and answer in an output
+
+        //adding thw question and answer in an output
          output.push(
-            // `<div class="questionbox"> ${currentQuestion.question} </div>
-            // <div class="btn"> ${answer.join('')} </div>`
-             ` ${currentQuestion.question} 
-             ${answer} `
+            `<div class="questionbox"> ${currentQuestion.question} </div>
+            <div class="answerbox"> ${answer.join('')} </div>`
           );
-        answerContainer.innerHTML = answer;
-        questionContainer.innerHTML = output;
+
 
     });
-
-    
+    quizContainer.innerHTML= output.join(''); 
 }
+function showResult() {
+    const answerContainers = quizContainer.querySelectorAll(".answer");
+    let correct = 0;
 
+    // for each question...
+  questions.forEach( (currentQuestion, questionNumber) => {
 
-function showAnswer() {
-    
+    // find selected answer
+    //const answerContainer = answerContainers[questionNumber];
+      //const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+      const picked = answersSelected.find(a => a.question === questionNumber);
+      if (picked.ans === currentQuestion.correctAnswer) {
+          correct++;
+          
+
+      }
+
+      // if answer is correct
+    //   if (userAnswer === currentQuestion.correctAnswer) {
+    //       // add to the number of correct answers
+    //       Correct++;
+    //       // color the answers green
+    //   answerContainers[questionNumber].style.color = 'lightgreen';
+    // }
+    // // if answer is wrong or blank
+    // else{
+    //   // color the answers red
+    //   answerContainers[questionNumber].style.color = 'red';
+    // }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
+let button = document.querySelectorAll("#btn");
+let answersSelected = []
 
-//To display quz question
-quiz();
+function pickAnswer(questionNumber, letter) {
+    let picked = {
+        question: que,
+        ans: answerPicked
+    }
+    answersSelected.push(answer);
 
-//On submit show the correct answer in green background and wrong answer in red background
-submitButon.addEventListener('click', showAnswer);
-
+}
