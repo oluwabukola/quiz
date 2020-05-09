e788084767d4af473898b4222b3e703ce0f55471
oluwabukola <daniellabaiyewu@gmail.com>
2020-05-09 18:37:44 +0100

    latest

--- script.js
+++ script.js
@@ -55,13 +55,14 @@ let questions = [
     }
 ];
 
-// const quizContainer = document.getElementById('quiz');
-// const resultsContainer = document.getElementById('results');
 
 
-// const submitButton = document.getElementById('submit');
-const quizContainer = document.querySelectorAll(".container");
+let slides = []
+let currentSlide = 0;
+const quizContainer = document.querySelector('#quiz');
 const submitButon = document.querySelector(".submit");
+const resultsContainer = document.querySelector("#congrat");
+const nextButton = document.querySelector(".button");
 
 function quiz() {
     
@@ -73,66 +74,120 @@ function quiz() {
         //for each aveilable answer
         for (letter in currentQuestion.answer) {
             answer.push(
-                
-                `<div id="btn" onclick="pickAnswer(${questionNumber}, ${letter}) >
+                `<div class="btnanswer" id="btn-${questionNumber}-${letter}" onclick="pickAnswer(${questionNumber}, '${letter}')" >
+                    <input type="hidden" name= "question${questionNumber}"  id="${questionNumber}-${letter}" value="${letter}">
                   ${currentQuestion.answer[letter]}
                 </div>`
-                
-              );
+            );
         }
 
         //adding thw question and answer in an output
          output.push(
-            `<div class="questionbox"> ${currentQuestion.question} </div>
-            <div class="answerbox"> ${answer.join('')} </div>`
+             `<div class="slide">
+             <div  class="container">
+             <p class="numquest">Question ${questionNumber + 1} of ${questions.length} </p>
+             <div class="style1"></div>
+            <div class="questionbox"> ${currentQuestion.question} </div>
+            <div class="answerbox"> ${answer.join('')} </div>
+           
+            
+    
+         
+            </div>
+            </div>`
+            
           );
 
-
     });
-    quizContainer.innerHTML= output.join(''); 
+    quizContainer.innerHTML= output.join('');
+    slides = document.querySelectorAll(".slide");
 }
-function showResult() {
-    const answerContainers = quizContainer.querySelectorAll(".answer");
-    let correct = 0;
 
-    // for each question...
+function showResult() {
+    console.log('Showing results...')
+    const answerContainers = quizContainer.querySelectorAll(".answerbox");
+    let correct = 0;
+     // for each question...
   questions.forEach( (currentQuestion, questionNumber) => {
 
-    // find selected answer
-    //const answerContainer = answerContainers[questionNumber];
-      //const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+
       const picked = answersSelected.find(a => a.question === questionNumber);
-      if (picked.ans === currentQuestion.correctAnswer) {
-          correct++;
-          
-
-      }
-
-      // if answer is correct
-    //   if (userAnswer === currentQuestion.correctAnswer) {
-    //       // add to the number of correct answers
-    //       Correct++;
-    //       // color the answers green
-    //   answerContainers[questionNumber].style.color = 'lightgreen';
-    // }
-    // // if answer is wrong or blank
-    // else{
-    //   // color the answers red
-    //   answerContainers[questionNumber].style.color = 'red';
-    // }
+      console.log(picked)
+       if (picked.ans === currentQuestion.correctAnswer) {
+           correct++;
+           const selector = `#btn-${questionNumber}-${picked.ans}`;
+           const correctDiv = document.querySelector(selector);
+           console.log(correctDiv)
+           correctDiv.style.backgroundColor = "green";
+       }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      const selector = `#btn-${questionNumber}-${picked.ans}`;
+      const correctDiv = document.querySelector(selector);
+      correctDiv.style.backgroundColor = "red";
+           const incorrectDiv = document.querySelector(`#btn-${questionNumber}-${currentQuestion.correctAnswer}`);
+           incorrectDiv.style.backgroundColor = 'green';
+           
+    }
   });
 
   // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    console.log(correct);
+  resultsContainer.innerHTML = `${correct} out of ${questions.length}`;
 }
-let button = document.querySelectorAll("#btn");
+// let button = document.querySelectorAll("#btn");
 let answersSelected = []
 
 function pickAnswer(questionNumber, letter) {
-    let picked = {
-        question: que,
-        ans: answerPicked
+        
+    if (answersSelected.some(item => item.question === questionNumber)){
+        let questionPicked = answersSelected.find(item => item.question === questionNumber);
+        questionPicked.ans = letter
+    } else {
+        let picked = {
+            question: questionNumber,
+            ans: letter
+        }
+        
+    answersSelected.push(picked);
     }
-    answersSelected.push(answer);
-
+    console.log(answersSelected)
 }
+
+function showSlide(n) {
+    console.log(n, currentSlide)
+    //console.log(slides)
+    const nextButton = document.querySelector(".button");
+    if (n !== 0)
+        slides[currentSlide].remove();
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    console.log(nextButton)
+    if(currentSlide === slides.length-1){
+        nextButton.style.display = "none";
+        
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+    
+    }
+}
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+function nextSubmit() {
+    const selector = `#btn-${questionNumber}-${picked.ans}`;
+           const correctDiv = document.querySelector(selector);
+    if (correctDiv) {
+        nextButton.style.diplay = "none";
+        
+    }
+      
+  }
+  
+//quiz();
+//showResult();
+showSlide(currentSlide);
+console.log(slides);
